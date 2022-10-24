01e497d73d68ccaf61b1e852f8aaf5c7cf374f89
cassiezzy <e0767624@u.nus.edu>
2022-10-24 15:19:41 +0800

    fix executionSeconds in postgre_client

--- driver/reader_test.go
+++ driver/reader_test.go
@@ -6,13 +6,18 @@ import (
 	"testing"
 )
 
-func TestSqlClient(t *testing.T) {
-	filePath := "../data/test_xact_files/test.txt"
-	SqlClient(filePath, 0)
-}
+//func TestSqlClient(t *testing.T) {
+//	filePath := "../data/test_xact_files/test.txt"
+//	SqlClient(filePath, 0)
+//}
 
 func TestSqlClient2(t *testing.T) {
 	s := "5"
 	i, _ := strconv.Atoi(s)
 	fmt.Println(s, i)
+}
+
+func TestNewOrder(t *testing.T) {
+	filePath := "../data/test_xact_files/test_psql_new_order.txt"
+	SqlClient(filePath, 0)
 }
