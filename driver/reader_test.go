4fb693d118755809c4630cd194b0f9a83698076f
cassiezzy <e0767624@u.nus.edu>
2022-10-26 20:33:14 +0800

    made tests for new_order, payment, delivery and order_status transactions

--- driver/reader_test.go
+++ driver/reader_test.go
@@ -11,7 +11,7 @@ import (
 //	SqlClient(filePath, 0)
 //}
 
-func TestSqlClient2(t *testing.T) {
+func TestSqlClient(t *testing.T) {
 	s := "5"
 	i, _ := strconv.Atoi(s)
 	fmt.Println(s, i)
@@ -19,5 +19,20 @@ func TestSqlClient2(t *testing.T) {
 
 func TestNewOrder(t *testing.T) {
 	filePath := "../data/test_xact_files/test_psql_new_order.txt"
+	SqlClient(filePath, 0)
+}
+
+func TestPayment(t *testing.T) {
+	filePath := "../data/test_xact_files/test_psql_payment.txt"
+	SqlClient(filePath, 0)
+}
+
+func TestDelivery(t *testing.T) {
+	filePath := "../data/test_xact_files/test_psql_delivery.txt"
+	SqlClient(filePath, 0)
+}
+
+func TestOrderStatus(t *testing.T) {
+	filePath := "../data/test_xact_files/test_psql_order_status.txt"
 	SqlClient(filePath, 0)
 }
