c0d8c192823f4ecba8e924b78d76e97e6e58bffc
cori-hwf <huangweifeng.cori@gmail.com>
2022-10-27 18:19:26 +0800

    reframe stock_level transaction

--- transaction/postgre/stock_level.go
+++ transaction/postgre/stock_level.go
@@ -3,11 +3,61 @@ package postgre
 import (
 	"cs5424project/store/models"
 	"fmt"
-	"gorm.io/gorm"
 	"log"
+
+	"gorm.io/gorm"
 )
 
 func StockLevel(warehouseId, districtId uint64, threshold, orderNumber int) error {
+
+	var err error
+	count := 0
+
+	district := &models.District{
+		WarehouseId: warehouseId,
+		Id:          districtId,
+	}
+	if err = db.First(district).Error; err != nil {
+		return err
+	}
+
+	err = db.Transaction(func(tx *gorm.DB) error {
+
+		nextOrderId := district.NextAvailableOrderNumber
+		startOrderId := int(nextOrderId) - orderNumber
+
+		var orderLines []models.OrderLine
+		if err = tx.
+			Where("warehouse_id = ? AND district_id = ? AND order_id >= ?", warehouseId, districtId, startOrderId).
+			Find(&orderLines).Error; err != nil {
+			return err
+		}
+
+		for _, orderLine := range orderLines {
+			itemId := orderLine.ItemId
+			stock := &models.Stock{
+				WarehouseId: warehouseId,
+				ItemId:      itemId,
+			}
+			if err = tx.First(stock).Error; err != nil {
+				return err
+			}
+			if stock.Quantity < threshold {
+				count += 1
+			}
+		}
+
+		fmt.Printf(" The total number of items in S where its stock quantity at W ID is below the threshold: %d\n", count)
+		return nil
+	})
+
+	if err != nil {
+		log.Printf("Stock level transaction error: %v", err)
+	}
+	return err
+}
+
+func StockLevel2(warehouseId, districtId uint64, threshold, orderNumber int) error {
 
 	err := db.Transaction(func(tx *gorm.DB) error {
 		var err error
