0b0120aa1667db56146ad0d27c68cd9a08be2e16
cassiezzy <e0767624@u.nus.edu>
2022-11-02 17:56:44 +0800

    tests updated

--- driver/reader_test.go
+++ driver/reader_test.go
@@ -1,7 +1,9 @@
 package driver
 
 import (
+	"cs5424project/store/postgre"
 	"fmt"
+	"github.com/stretchr/testify/assert"
 	"strconv"
 	"testing"
 )
@@ -35,4 +37,31 @@ func TestDelivery(t *testing.T) {
 func TestOrderStatus(t *testing.T) {
 	filePath := "../data/test_xact_files/test_psql_order_status.txt"
 	SqlClient(filePath, 0)
+}
+
+func TestStocklevel(t *testing.T) {
+	filePath := "../data/test_xact_files/test_psql_stock_level.txt"
+	SqlClient(filePath, 0)
+}
+
+func TestPopularItem(t *testing.T) {
+	filePath := "../data/test_xact_files/test_psql_popular_item.txt"
+	SqlClient(filePath, 0)
+}
+
+func TestTopBalance(t *testing.T) {
+	filePath := "../data/test_xact_files/test_psql_top_balance.txt"
+	SqlClient(filePath, 0)
+}
+
+func TestRelatedCustomer(t *testing.T) {
+	filePath := "../data/test_xact_files/test_psql_related_customer.txt"
+	SqlClient(filePath, 0)
+}
+
+func Test1(t *testing.T) {
+	db := postgre.GetDB()
+	var orderNumber uint64
+	db.Exec("SELECT min(id) from orders where carrier_id = 0 AND warehouse_id = ? AND district_id = ?", 1, 1).Row().Scan(&orderNumber)
+	assert.Equal(t, true, true, orderNumber)
 }
