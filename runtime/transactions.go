37bd194c4dd71908f04577b0e4517c1efa6a8d7d
Jiayuli-CU <jiayu.li@columbia.edu>
2022-11-06 15:42:04 +0800

    feat: add program entrance logics

--- runtime/transactions.go
+++ runtime/transactions.go
@@ -1,17 +1,20 @@
 package runtime
 
-import "fmt"
+import (
+	"cs5424project/driver"
+	"fmt"
+	"sync"
+)
 
 func StartTransactions(n1, n2, n3, n4 int) {
 
-	fmt.Println(n1, n2, n3, n4)
-	//var wg sync.WaitGroup
-	//
-	//for _, i := range []int{n1, n2, n3, n4} {
-	//	wg.Add(1)
-	//	filePath := fmt.Sprintf("data/xact_files/%d.txt", i)
-	//	go driver.SqlClient(&wg, filePath, i)
-	//}
-	//
-	//wg.Wait()
+	var wg sync.WaitGroup
+
+	for _, i := range []int{n1, n2, n3, n4} {
+		wg.Add(1)
+		filePath := fmt.Sprintf("data/xact_files/%d.txt", i)
+		go driver.SqlClient(&wg, filePath, i)
+	}
+
+	wg.Wait()
 }
